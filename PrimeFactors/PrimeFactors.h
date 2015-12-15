4f463ca1f0e07098d93520f12b539c5706c9534a
Alex Cristea <alex@thinslices.com>
2015-12-15 19:22:15 +0200

    Initial Commit

---
+++ PrimeFactors/PrimeFactors.h
@@ -0,0 +1,19 @@
+//
+//  PrimeFactors.h
+//  PrimeFactors
+//
+//  Created by Alex Cristea on 15/12/15.
+//  Copyright © 2015 iOS NSAgora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrimeFactors.
+FOUNDATION_EXPORT double PrimeFactorsVersionNumber;
+
+//! Project version string for PrimeFactors.
+FOUNDATION_EXPORT const unsigned char PrimeFactorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrimeFactors/PublicHeader.h>
+
+
